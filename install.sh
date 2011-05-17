4e79899b9cff8fd28b2262fccdeac0642ba01444
Pierre Quillery <pierre@quillery.fr>
2011-05-17 00:26:06 -0700

    Changing the "~" character to $HOME for better readability.

--- install.sh
+++ install.sh
@@ -7,7 +7,7 @@
 # HANDLING OF THE LANG FILE
 
 # This is the directory where we need to put the syntax file.
-rep=~/.local/share/gtksourceview-2.0/language-specs
+rep=$HOME/.local/share/gtksourceview-2.0/language-specs
 
 # We create it if it doesn't exist yet.
 mkdir -p $rep
@@ -20,10 +20,10 @@ cp ./smarty.lang "$rep/smarty.lang"
 # HANDLING OF THE XML FILE
 # We need to declare a new mime type.
 
-rep_mime=~/.local/share/mime/packages
+rep_mime=$HOME/.local/share/mime/packages
 mkdir -p $rep_mime
 cp ./smarty.xml "$rep_mime/smarty.xml"
 
 # We need to update the mime database to allow the handling 
 # of smarty template files.
-update-mime-database ~/.local/share/mime
+update-mime-database $HOME/.local/share/mime
